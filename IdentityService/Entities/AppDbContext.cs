d32a5209d6e6e16a657ddc3ae368c5c99aea8237
Jakub Kubacki <j.kubacki@outlook.com>
2019-05-23 19:58:13 +0200

    Setup identity

---
+++ IdentityService/Entities/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace IdentityService.Entities
+{
+    public class AppDbContext: IdentityDbContext<AppUser>
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+          : base(options)
+        {
+        }
+    }
+}
