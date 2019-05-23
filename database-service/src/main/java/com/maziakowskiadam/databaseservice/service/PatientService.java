359213a569381008ec0eeb16cc453cdfa2387758
Adam <maziakowskiadam@gmail.com>
2019-05-23 22:41:27 +0200

    commit

--- database-service/src/main/java/com/maziakowskiadam/databaseservice/service/PatientService.java
+++ database-service/src/main/java/com/maziakowskiadam/databaseservice/service/PatientService.java
@@ -120,8 +120,6 @@ public class PatientService {
     public String editPatient(Long id, RegisterPatientDto registerPatientDto) {
         try {
 
-            Optional<Patient> optionalPatient = patientRepository.findById(id);
-            System.out.println(optionalPatient.get());
             Patient oldPatient = patientRepository.findById(id).get();
 
             oldPatient.setFirstName(registerPatientDto.getFirstName());
@@ -143,9 +141,8 @@ public class PatientService {
             Optional<Address> optionalAddress = addressRepository
                     .findAddressByStreetAndHouseAndZipcodeAndCity(street, house, zipcode, city);
 
-            if (optionalAddress.isPresent()) {
-                System.out.println("adres istnieje");
-            } else {
+            if (!optionalAddress.isPresent()) {
+
                 addressService.addAddress(address);
                 Address newAddress = addressRepository.findAddressByStreetAndHouseAndZipcodeAndCity(street, house, zipcode, city).get();
                 oldPatient.setAddress(newAddress);
