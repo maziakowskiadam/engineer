2fbe1669ae4acef80b076fd2d0c41535412c8a03
Adam <maziakowskiadam@gmail.com>
2019-05-28 16:54:32 +0200

    Finally centered the pissing login form

--- mcc-app/src/app/pages/login-page/login-page.component.ts
+++ mcc-app/src/app/pages/login-page/login-page.component.ts
@@ -2,8 +2,12 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'app-login-page',
-    templateUrl: './login-page.component.html'
+    templateUrl: './login-page.component.html',
+    styleUrls: ['./login-page.component.scss']
 })
 export class LoginPageComponent {
 
+    onSubmit(form) {
+        console.log(form);
+    }
 }
