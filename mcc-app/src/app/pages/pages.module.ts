5166fe2dcd1bdff3b82dea6b022154577a3a93cf
Adam <maziakowskiadam@gmail.com>
2019-05-31 22:32:21 +0200

    done nothing new

--- mcc-app/src/app/pages/pages.module.ts
+++ mcc-app/src/app/pages/pages.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { LoginPageComponent } from './login-page/login-page.component';
 import { DoctorsPageComponent } from './doctors-page/doctors-page.component';
 import { PatientPageComponent } from './patient-page/patient-page.component';
-import { UnauthorizedPageComponent } from './unauthorized-page/unauthorized-page.component';
+// import { UnauthorizedPageComponent } from './unauthorized-page/unauthorized-page.component';
 import { ManagementPageComponent } from './management-page/management-page.component';
 import { DefaultPageComponent } from './default-page/default-page.component';
 import { CommonModule } from '@angular/common';
@@ -12,7 +12,7 @@ import { FormsModule } from '@angular/forms';
 import { AppointmentsPageComponent } from './appointments-page/appointments-page.component';
 import { ResultsPageComponent } from './results-page/results-page.component';
 import { PasswordRecoveryPageComponent } from './password-recovery-page/password-recovery-page.component';
-import { AccountSettingsPageComponent } from './account-settings-page/account-settings-page.component';
+// import { AccountSettingsPageComponent } from './account-settings-page/account-settings-page.component';
 import { NewAppointmentPageComponent } from './new-appointment-page/new-appointment-page.component';
 import { AddDoctorPageComponent } from './add-doctor-page/add-doctor-page.component';
 import { AddRoomPageComponent } from './add-room-page/add-room-page.component';
@@ -23,7 +23,7 @@ import { ActivateAccountPageComponent } from './activate-account-page/activate-a
         LoginPageComponent,
         DoctorsPageComponent,
         PatientPageComponent,
-        UnauthorizedPageComponent,
+        // UnauthorizedPageComponent,
         ManagementPageComponent,
         DefaultPageComponent,
         AddPatientPageComponent,
@@ -31,7 +31,7 @@ import { ActivateAccountPageComponent } from './activate-account-page/activate-a
         AppointmentsPageComponent,
         ResultsPageComponent,
         PasswordRecoveryPageComponent,
-        AccountSettingsPageComponent,
+        // AccountSettingsPageComponent,
         NewAppointmentPageComponent,
         AddDoctorPageComponent,
         AddRoomPageComponent,
@@ -45,7 +45,7 @@ import { ActivateAccountPageComponent } from './activate-account-page/activate-a
         LoginPageComponent,
         DoctorsPageComponent,
         PatientPageComponent,
-        UnauthorizedPageComponent,
+        // UnauthorizedPageComponent,
         ManagementPageComponent,
         DefaultPageComponent,
         AddPatientPageComponent
