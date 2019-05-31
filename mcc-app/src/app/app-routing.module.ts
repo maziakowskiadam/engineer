1967a39329aa4c358d123ed1d9d0a6b80484426e
Adam <maziakowskiadam@gmail.com>
2019-05-31 22:38:38 +0200

    done nothing

--- mcc-app/src/app/app-routing.module.ts
+++ mcc-app/src/app/app-routing.module.ts
@@ -1,27 +1,5 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-<<<<<<< HEAD
-import { LoginPageComponent } from './pages/login-page/login-page.component';
-import { DoctorsPageComponent } from './pages/doctors-page/doctors-page.component';
-import { PatientPageComponent } from './pages/patient-page/patient-page.component';
-// import { UnauthorizedPageComponent } from './pages/unauthorized-page/unauthorized-page.component';
-import { ManagementPageComponent } from './pages/management-page/management-page.component';
-import { DefaultPageComponent } from './pages/default-page/default-page.component';
-import { PagesModule } from './pages/pages.module';
-import { AddPatientPageComponent } from './pages/add-patient-page/add-patient-page.component';
-import { AddAppointmentPageComponent } from './pages/add-appointment-page/add-appointment-page.component';
-import { IdentityGuard } from './guards/identity.guard';
-import { RouteUrl } from './constants/route.constants';
-import { AppointmentsPageComponent } from './pages/appointments-page/appointments-page.component';
-import { ResultsPageComponent } from './pages/results-page/results-page.component';
-import { PasswordRecoveryPageComponent } from './pages/password-recovery-page/password-recovery-page.component';
-// import { AccountSettingsPageComponent } from './pages/account-settings-page/account-settings-page.component';
-import { NewAppointmentPageComponent } from './pages/new-appointment-page/new-appointment-page.component';
-import { AddDoctorPageComponent } from './pages/add-doctor-page/add-doctor-page.component';
-import { AddRoomPageComponent } from './pages/add-room-page/add-room-page.component';
-import { ActivateAccountPageComponent } from './pages/activate-account-page/activate-account-page.component';
-=======
->>>>>>> 6cfd958e340337aa35093ce5329441fa1bc8535b
 
 // const routes: Routes = [
 //     {
