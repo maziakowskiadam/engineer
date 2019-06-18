c2d0f7c8e5711f91fb55e00d2dc908979cc208ec
Jakub Kubacki <j.kubacki@outlook.com>
2019-06-18 23:14:36 +0200

    29 Authorize

--- mcc-app/src/app/app-routing.module.ts
+++ mcc-app/src/app/app-routing.module.ts
@@ -1,75 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-
-// const routes: Routes = [
-//     {
-//         path: RouteUrl.LOGIN,
-//         component: LoginPageComponent
-//     },
-//     {
-//         path: 'password-recovery',
-//         component: PasswordRecoveryPageComponent
-//     },
-//     {
-//         path: RouteUrl.DOCTORS,
-//         component: DoctorsPageComponent,
-//         // canActivate: [IdentityGuard],
-//     },
-//     {
-//         path: RouteUrl.PATIENTS,
-//         component: PatientPageComponent,
-//         // canActivate: [IdentityGuard],
-//     },
-//     {
-//         path: RouteUrl.UNAUTHORIZED,
-//         component: UnauthorizedPageComponent
-//     },
-//     {
-//         path: RouteUrl.MANAGEMENT,
-//         component: ManagementPageComponent
-//     },
-//     {
-//         path: 'add-doctor',
-//         component: AddDoctorPageComponent
-//     },
-//     {
-//         path: 'add-room',
-//         component: AddRoomPageComponent
-//     },
-//     {
-//         path: 'add-patient',
-//         component: AddPatientPageComponent
-//     },
-//     {
-//         path: 'activate-account',
-//         component: ActivateAccountPageComponent
-//     },
-//     {
-//         path: 'add-appointment',
-//         component: AddAppointmentPageComponent
-//     },
-//     {
-//         path: 'appointments',
-//         component: AppointmentsPageComponent
-//     },
-//     {
-//         path: 'results',
-//         component: ResultsPageComponent
-//     },
-//     {
-//         path: 'account-settings',
-//         component: AccountSettingsPageComponent
-//     },
-//     {
-//         path: 'new-appointment',
-//         component: NewAppointmentPageComponent
-//     },
-//     {
-//         path: '',
-//         component: DefaultPageComponent,
-//         pathMatch: 'full'
-//     }
-// ];
+import { ManagementGuardService } from './modules/management/management-guard.service';
+import { DoctorGuardService } from './modules/doctor/doctor-guard.service';
 
 const routes: Routes = [
     {
@@ -82,19 +14,18 @@ const routes: Routes = [
     },
     {
         path: 'management',
-        loadChildren: () => import('./modules/management/management.module').then(mod => mod.ManagementModule)
+        loadChildren: () => import('./modules/management/management.module').then(mod => mod.ManagementModule),
+        canActivate: [ManagementGuardService]
     },
     {
         path: 'doctor',
-        loadChildren: () => import('./modules/doctor/doctor.module').then(mod => mod.DoctorModule)
+        loadChildren: () => import('./modules/doctor/doctor.module').then(mod => mod.DoctorModule),
+        canActivate: [DoctorGuardService]
     }
 ];
 
 @NgModule({
-    // declarations: [
-    // ],
     imports: [
-        // PagesModule,
         RouterModule.forRoot(routes)
     ],
     exports: [
